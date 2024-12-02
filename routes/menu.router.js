3c3f3e151e2cf791ddfe29c3c1cf1040f0b7264d
nadaelnegihi <168196704+nadaelnegihi@users.noreply.github.com>
2024-12-02 20:12:03 +0200

    Nada (#3)
    * nada
    
    * all apis are working
    
    * few adjustments
    
    ---------
    
    Co-authored-by: batool612 <128169045+batool612@users.noreply.github.com>

--- routes/menu.router.js
+++ routes/menu.router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { addMenuItem, editMenuItem, deleteMenuItem } = require("../controller/menuController");
+const { addMenuItem, editMenuItem, deleteMenuItem ,getAllMenuItems} = require("../controller/menuController");
 const { auth, Roles } = require("../middleware/authMiddleware"); 
 
 // Add Menu Item (Admin Only)
@@ -11,5 +11,6 @@ router.put("/edit/:itemId", auth([Roles.Admin]), editMenuItem);
 
 // Delete Menu Item (Admin Only)
 router.delete("/delete/:itemId", auth([Roles.Admin]), deleteMenuItem);
-
+// Get Menu Items
+router.get('/', auth([Roles.User, Roles.Admin]), getAllMenuItems);
 module.exports = router;
--- routes/reservation.router.js
+++ routes/reservation.router.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { createReservation, editReservation,cancelReservation, manageReservation,} = require('../controller/reservationController');
+const { createReservation, editReservation,cancelReservation, manageReservation,getAllReservations} = require('../controller/reservationController');
 const { auth, Roles } = require('../middleware/authMiddleware');
 
 
 router.patch('/cancel/:reservationId', cancelReservation); 
-
-
+router.get('/', getAllReservations); 
 // User Routes
 router.post('/create', auth([Roles.User]), createReservation); 
 router.put('/edit/:reservationId', auth([Roles.User]), editReservation); 
 
 
+
 // Admin Routes
 router.put('/manage/:reservationId', auth([Roles.Admin]), manageReservation); // Admin Manage Reservation (Edit or Cancel)
+
+
 
 module.exports = router;
